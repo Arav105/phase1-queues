b0cd85fdfe1e7179385e72cd0b7f3cdc7f5202f9
Arav105 <chachuarav@gmail.com>
2023-07-26 21:44:06 +0530

    phase1 queues

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phase1_queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        public static void runApp()
+        {
+            Queues Q = new Queues(10);
+
+            Q.insert(10);
+            Q.insert(20);
+            Q.insert(30);
+            Q.insert(40);
+            Q.insert(50);
+            Q.insert(60);
+            Q.insert(70);
+            Q.insert(80);
+            Q.insert(90);
+            Q.insert(100);
+
+            Console.WriteLine(Q.printQueues());
+
+            Q.delete();
+            Q.delete();
+
+            Console.WriteLine(Q.printQueues());
+            Console.ReadKey();
+        }
+
+    }
+}
